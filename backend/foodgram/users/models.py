c5259de2e734b257c37c6ba9850bb6e0e06a584b
вашÐstupidcabbage <vanya@gutche.ru>
2023-05-29 19:20:16 +0500

    Add more readable message for test

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -13,13 +13,16 @@ class User(AbstractUser):
             RegexValidator(regex=re.compile(r"^[\w.@+-]+\Z"),
                            message='Проверьте правильность написания никнейма')
         ])
+    last_name = models.CharField(
+        'Фамилия пользователя',
+        max_length=150)
     first_name = models.CharField(
         'Имя пользователя',
         max_length=150)
-    last_name = models.CharField(
-        'Фамилия пользователя',
-        max_length=150),
     email = models.EmailField(
         'Электронная почта',
         max_length=254,
         unique=True)
+
+    def __str__(self):
+        return f'{self.username}: {self.email}'
--- backend/foodgram/users/tests/test_models.py
+++ backend/foodgram/users/tests/test_models.py
@@ -8,7 +8,7 @@ class UserModelTest(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.user = User.objects.create(
+        cls.user = User.objects.create_user(
             email='test@test.com',
             username='testuser',
             first_name='first_test',
@@ -19,10 +19,14 @@ class UserModelTest(TestCase):
     def test_models_have_correct_objects_name(self):
         """Проверяем корректиность работы __str__."""
         user = UserModelTest.user
-        expected_objects_name = user.username
-        self.assertEqual(expected_objects_name, str(user))
+        expected_objects_name = f'{user.username}: {user.email}'
+        self.assertEqual(expected_objects_name, str(user),
+                         msg='Отображение __str__ работает некорректно!\n' +
+                         'Может вы не указали метод __str__?\n' +
+                         'Проверьте, соотвествие форме: {username: email}')
 
     def test_models_have_index(self):
         """Проверяем наличие индексации у моделей."""
         user = UserModelTest.user
-        self.assertIsNotNone(user.id)
+        self.assertIsNotNone(user.id,
+                             msg='Индексация у пользователей отсутсвует!')
