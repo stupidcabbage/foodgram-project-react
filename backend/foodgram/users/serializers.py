6ef156994c465d947b8fff01ff1272d4b1ab599d
вашÐstupidcabbage <vanya@gutche.ru>
2023-06-03 23:09:46 +0500

    Ready test for user registration

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -5,5 +5,10 @@ from .models import User
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name', 'password',)
+        fields = ('email',
+                  'id',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'password',)
         read_only_fields = ('id',)
--- backend/foodgram/users/tests/test_api.py
+++ backend/foodgram/users/tests/test_api.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from users.models import User
 
+FIRST_ENDPOINT_ERROR = 0
+
 
 class UserTest(APITestCase):
     def test_create_account(self):
@@ -40,3 +42,51 @@ class UserTest(APITestCase):
         """
         Провеярем наличие ошибки при использовании невалидных длинных значений.
         """
+        url = reverse('register')
+        data = {'email': f'{"a"*245}@test.test',
+                'username': 'a'*151,
+                'first_name': 'a'*151,
+                'last_name': 'a'*151,
+                'password': 'test123123'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         'При отправлении некорректных данных эндпойнт' +
+                         'должен возвращать 400_BAD_REQUEST')
+        self.assertEqual(User.objects.count(), 0,
+                         'Аккаунт сохраняется в базе данных, ' +
+                         'после неуспешного ответа от эндпойнта.')
+
+        del data['password']
+        for field in data:
+            with self.subTest(field=field):
+                self.assertEqual(
+                    response.data.get(field)[FIRST_ENDPOINT_ERROR].code,
+                    'max_length',
+                    f'Провеьте, что при отправлении длинного поля {field},' +
+                    ' эндпойнт возвращает ошибку')
+
+    def test_create_dublicate_account(self):
+        """
+        Проверяем отсутствие возможности создать два одиноковых аккаунтов.
+        """
+        url = reverse('register')
+        data = {'email': 'testemail@test.com',
+                'username': 'testuser',
+                'first_name': 'first_test',
+                'last_name': 'second_test',
+                'password': 'test123123'}
+        self.client.post(url, data, format='json')
+        second_response = self.client.post(url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 1,
+                         'Проверьте, что аккаунт дубликат не может быть создан'
+                         )
+
+        for field in ('username', 'email'):
+            with self.subTest(field=field):
+                self.assertEqual(
+                    second_response.data.get(field)[FIRST_ENDPOINT_ERROR].code,
+                    'unique',
+                    'Проверьте, что невозможно создать аккаунты' +
+                    'с одинаковыми полями и эндпойнт возвращает ошибку.'
+                )
