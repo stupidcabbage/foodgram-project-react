0667e12146c17812a12317d1c461b2f8f3629d3f
вашÐstupidcabbage <vanya@gutche.ru>
2023-06-11 01:01:45 +0500

    Fix path

--- backend/foodgram/food/management/commands/upload_data.py
+++ backend/foodgram/food/management/commands/upload_data.py
@@ -1,15 +1,14 @@
 import csv
 from django.core.management.base import BaseCommand
 from food.models import Ingridient
-import os
-
-path = r'F:/Dev_Ya/diplom/foodgram-project-react/data/ingredients.csv'
-assert os.path.isfile(path)
+from foodgram.settings import BASE_DIR
 
 
 def upload_ingridients():
     """Загружает ингридиенты в БД."""
-    with open(path, encoding='utf-8') as csv_file:
+    with open(
+              BASE_DIR.parent.parent / 'data/ingredients.csv',
+              encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
 
         for row in csv_reader:
